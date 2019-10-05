fa0b220bc0294400e7140dd082fec09fb4247aa9
Joao Ubaldo Xavier Reis <joao.reis21@gmail.com>
2019-10-05 18:36:23 -0300

    Reconhecimento de voz em texto

--- app/src/main/java/br/com/joaoreis/arduinowatson/MainActivity.kt
+++ app/src/main/java/br/com/joaoreis/arduinowatson/MainActivity.kt
@@ -1,12 +1,53 @@
 package br.com.joaoreis.arduinowatson
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.speech.RecognizerIntent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import br.com.joaoreis.arduinowatson.databinding.ActivityMainBinding
+
+private const val SPEECH_REQUEST_CODE = 0
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        binding.btGravar.setOnClickListener {
+            displaySpeechRecognizer()
+        }
+    }
+
+    private fun displaySpeechRecognizer() {
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+            putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+            )
+        }
+        // Start the activity, the intent will be populated with the speech text
+        startActivityForResult(intent, SPEECH_REQUEST_CODE)
+    }
+
+    // This callback is invoked when the Speech Recognizer returns.
+    // This is where you process the intent and extract the speech text from the intent.
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == SPEECH_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            val spokenText =
+                data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS).let { results ->
+                    results?.get(0)
+                }
+            // Do something with spokenText
+            binding.tvTexto.text = spokenText
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
