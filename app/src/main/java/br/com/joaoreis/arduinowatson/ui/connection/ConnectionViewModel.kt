a0c226e378ebd9925c56db63686095cd6996c75e
Joao Ubaldo Xavier Reis <joao.reis21@gmail.com>
2019-10-05 23:34:11 -0300

    Renaming classes and adding colors

--- app/src/main/java/br/com/joaoreis/arduinowatson/ui/connection/ConnectionViewModel.kt
+++ app/src/main/java/br/com/joaoreis/arduinowatson/ui/connection/ConnectionViewModel.kt
@@ -1,10 +1,10 @@
-package br.com.joaoreis.arduinowatson.ui.home
+package br.com.joaoreis.arduinowatson.ui.connection
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class ConnectionViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
