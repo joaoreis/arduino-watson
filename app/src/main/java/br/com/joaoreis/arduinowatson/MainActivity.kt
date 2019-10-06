fb4db1e25cfcd7621578085f572cad065fae7f9b
Joao Ubaldo Xavier Reis <joao.reis21@gmail.com>
2019-10-05 21:30:02 -0300

    Gradle dependencies

--- app/src/main/java/br/com/joaoreis/arduinowatson/MainActivity.kt
+++ app/src/main/java/br/com/joaoreis/arduinowatson/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setupListeners()
     }
 
